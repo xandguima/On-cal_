f037e0bb5cbfa2f74aa97b0504faa93090008b39
xandguima <xan.96@hotmail.com>
2023-12-01 19:58:05 -0300

    create functions for localization routes

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,63 @@
 import daysContainer from "./controller/daysContainerController.js"
+import notesDays from "./controller/notesController.js"
 
-daysContainer.createDays()
-daysContainer.buttonsScroll()
+
+const routes = {
+  "/": "src/pages/dashboard.html",
+  "/notification":"src/pages/notification.html",
+  "/addNotes":"src/pages/addNotes.html",
+  "/settings":"src/pages/settings.html"
+ 
+}
+
+function route(event) {
+  event.preventDefault()
+  
+  window.history.pushState({},"",event.target.href)
+  handle()
+
+}
+async function handle() {
+  const {pathname}=window.location
+  console.log(pathname)
+  
+  const route = routes[pathname] || routes[404]
+  console.log(route)
+  await fetch(route)
+  .then(data => data.text())
+  .then(html => {
+    const styleSheet = document.querySelector("#css")
+    document.querySelector('#overall').innerHTML = html
+    
+    if(pathname=="/settings"){
+      styleSheet.href = "src/css/settings.css"
+
+    }else if (pathname == "/addNotes") {
+      styleSheet.href = "src/css/addNotes.css"
+    }
+    else if (pathname == "/notification") {
+      styleSheet.href = "src/css/notification.css"
+    }
+    else if(pathname=="/") {      
+      daysContainer.createDays()
+      notesDays.toggleShowNotes()
+     daysContainer.dateDashboard()
+     styleSheet.href = "src/css/dashboard.css"
+    }
+
+  })
+  
+  
+}
+window.onpopstate=()=>handle()
+window.route = route;
+
+
+
+
+
+
+
+
+
+//daysContainer.buttonsScroll()
